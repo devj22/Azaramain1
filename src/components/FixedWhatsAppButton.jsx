afda99e5539ab951fd09dc89de662253361ef4d2
Dev Jangid <devsthr123@gmail.com>
2025-03-09 18:34:17 +0530

    Describe your changes here

--- src/components/FixedWhatsAppButton.jsx
+++ src/components/FixedWhatsAppButton.jsx
@@ -8,7 +8,10 @@ const FixedWhatsAppButton = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <button className="w-12 h-12 flex items-center justify-center bg-green-500 hover:bg-green-600 rounded-full shadow-lg">
+        <button className="w-12 h-12 flex items-center justify-center 
+             bg-[#1BD742] 
+             hover:bg-[#17c439] 
+             rounded-full shadow-lg">
           <img
             src="https://i.pinimg.com/736x/bf/8a/76/bf8a76719f900b8757154eb3cfbc844a.jpg"
             alt="WhatsApp Logo"
